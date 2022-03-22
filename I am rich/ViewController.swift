76267552cc42a61bbe5fa10dda61f568ed6456df
Shahzad Umar Baig <ShahzadUBaig@gmail.com>
2022-03-22 17:03:33 +0500

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by MacbookPro2015 on 22/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
